fd8bbbb1cbc0263d6081bb1da0cf7302b0e06325
Harsh Paryani <harshparyani68@gmail.com>
2025-04-29 17:26:14 +0530

    Connected Mongo DB to the project

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config();
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+dotenv.config(
+    {path: "./env"}
+);
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+// 2nd way to connect to MongoDB using async/await
+/*
+    import mongoose from 'mongoose';
+    import {DB_NAME} from './constants.js';
+   import express from "express";
+   const app = exxpress();
+   ( async() => {
+        try{
+        await mognoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on('error', (err) => {
+            console.log(err);
+            throw err;
+        }
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+
+        catch(err){
+            console.log(err);
+            throw err;
+        } ) ()
+ */
