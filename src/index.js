0d868dbf79b17fa2b9e6260e6dd2ff3dad846f43
Harsh Paryani <harshparyani68@gmail.com>
2025-04-30 16:41:10 +0530

    utlis Code Updation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ dotenv.config(
     {path: "./env"}
 );
 
-connectDB();
+connectDB()
+.then(() => {
+   app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err.message);
+    throw err;
+});
 
 
 
