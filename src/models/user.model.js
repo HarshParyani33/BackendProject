5407d507dc5bb7da0f7fdbd5edb74dbac7390782
Harsh Paryani <harshparyani68@gmail.com>
2025-05-01 14:53:34 +0530

    Fixed minor errors in app.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,11 +54,11 @@ userSchema.pre("save", async function (next) {
 } )
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    await bcrypt.compare(password, this.password)
+   return  await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function () {
-    jwt.sign({
+    return jwt.sign({
             _id: this._id,
             username: this.username,
             email: this.email,
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = function () {
     )
 }
 userSchema.methods.generateRefreshToken = function () {
-    jwt.sign({
+    return jwt.sign({
             _id: this._id,
         },
         process.env.REFRESH_TOKEN_SECRET, {
