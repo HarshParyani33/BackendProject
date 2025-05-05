d7244913b45e06325aaf7bcd33f3fda17a20748b
Harsh Paryani <harshparyani68@gmail.com>
2025-05-05 18:06:31 +0530

    Add tweet like functionality with validation and error handlingand getAllLikedVideos functionallity

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,6 +5,7 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {Video} from "../models/video.model.js"
 import {Comment} from "../models/comment.model.js" 
+import {Tweet} from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
@@ -58,7 +59,7 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
         throw new ApiError(404, "No comment Found")
     }
 
-    const like  = await Like.findById({ userId,commentId})
+    const like  = await Like.findOne({ userId,commentId})
 
     if (like) {
         await like.deleteOne({userId,commentId})
@@ -81,11 +82,54 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+    const {userId} = req.user
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid User Id")
+        
+    }
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid Tweet Id")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if (!tweet) {
+        throw new ApiError(404, "No Tweet Found")
+    }
+    const like = await Like.findOne({userId,tweetId})
+    if (like) {
+        await like.deleteOne({userId,tweetId})
+        tweet.likesCount -= 1  
+        await tweet.save()
+        return res  
+        .status(200)
+        .json(new ApiResponse(200,like,"Tweet Disliked Successfully"))
+    } 
+    else {
+        await Like.create({userId,tweetId})
+        tweet.likesCount += 1
+        await tweet.save()
+        return res
+        .status(200)
+        .json(new ApiResponse(200,like,"Tweet Liked Successfully"))
+    }
+})
  
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const {userId} = req.user
+
+    if (!userId) {
+        throw new ApiError(400,"User Not found")
+    }
+    const likedVideos = await Like.find({userId}).populate("videoId")
+    if (!likedVideos) {
+        throw new ApiError(404,"No liked videos found")
+    }
+    res
+    .status(200)
+    .json(new ApiResponse(200,likedVideos,"Liked Videos Fetched Successfully"))
+
+
+
 })
  
 export {
